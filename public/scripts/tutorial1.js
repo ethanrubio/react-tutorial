f092b370089581cfeb4d4f5fbddfc955acf52e8f
Ethan Rubio <ethanrubio@gmail.com>
2016-04-12 12:30:48 -0700

    [feature] adds JSX component version of commentbox

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -1,13 +1,28 @@
 var CommentBox = React.createClass({
   render: function() {
     return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
+      React.createElement('div', {className: "commentBox"},
+        "Hello, world! I am a CommentBox"
+      )
     );
   }
 });
 ReactDOM.render(
-  <CommentBox />,
+  React.createElement(CommentBox, null),
   document.getElementById('content')
 );
+
+// non-JSX
+// var CommentBox = React.createClass({
+//   render: function() {
+//     return (
+//       <div className="commentBox">
+//         Hello, world! I am a CommentBox.
+//       </div>
+//     );
+//   }
+// });
+// ReactDOM.render(
+//   <CommentBox />,
+//   document.getElementById('content')
+// );
